6502d03a0250ace22029e9afbb4119a8889f1a09
Alexandru Bereghici <abereghici@tripadvisor.com>
2023-02-05 20:39:24 +0200

    chore: fix e2e test

--- e2e-tests/themes.spec.ts
+++ e2e-tests/themes.spec.ts
@@ -6,14 +6,16 @@ test('toggling the theme', async ({page}) => {
   const html = () => page.locator('html')
   const toggler = () => page.getByRole('button', {name: 'Toggle theme'})
 
-  expect(html()).toHaveAttribute('data-theme', 'light')
+  const themeAttribute = 'data-theme'
+  const initialTheme = await html().getAttribute(themeAttribute)
+  const oppositeTheme = initialTheme === 'light' ? 'dark' : 'light'
 
   await toggler().click()
-  expect(html()).toHaveAttribute('data-theme', 'dark')
+  expect(html()).toHaveAttribute(themeAttribute, oppositeTheme)
 
   await page.reload()
 
   await expect(toggler()).toBeVisible()
 
-  await expect(html()).toHaveAttribute('data-theme', 'dark')
+  await expect(html()).toHaveAttribute(themeAttribute, oppositeTheme)
 })
